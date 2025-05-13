8a44f22b1b16738e6ab063d82c785ff89b575c4a
ToninoJoya <antoniocaprab@gmail.com>
2025-05-13 17:14:15 +0000

    estructura paginas

---
+++ src/components/Planets.jsx
@@ -0,0 +1,23 @@
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+
+export const Planets = () => {
+
+    return (
+        <div className="container mt-5">
+            <h2 className="text-danger">Planets</h2>
+            <div className="d-flex flex-row flex-nowrap overflow-auto py-3">
+                <div className="card me-3 flex-shrink-0" style={{ width: '18rem' }}>
+                    <img src={rigoImageUrl} className="card-img-top" alt="Card Unica" style={{ height: '150px', objectFit: 'cover' }} />
+                    <div className="card-body">
+                        <h5 className="card-title">Card Title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <div className="d-flex justify-content-between">
+                            <a href="#" className="btn btn-outline-primary">Go somewhere </a><button type="button" class="btn btn-outline-warning">@</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { Planets } from "../components/Planets.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
@@ -6,15 +7,23 @@ export const Home = () => {
 	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<div className="card d-flex flex-columm">
-				<img src={rigoImageUrl} className="card-img-top w-25"  alt="..."/>
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-						<a href="#" className="btn btn-outline-primary">Go somewhere</a>
+		<>
+			<div className="container mt-5">
+				<h2 className="text-danger">Characters</h2>
+				<div className="d-flex flex-row flex-nowrap overflow-auto py-3">
+					<div className="card me-3 flex-shrink-0" style={{ width: '18rem' }}>
+						<img src={rigoImageUrl} className="card-img-top" alt="Card Unica" style={{ height: '150px', objectFit: 'cover' }} />
+						<div className="card-body">
+							<h5 className="card-title">Card Title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+							<div className="d-flex justify-content-between">
+								<a href="#" className="btn btn-outline-primary">Go somewhere </a><button type="button" class="btn btn-outline-warning">@</button>
+							</div>
+						</div>
 					</div>
+				</div>
 			</div>
-		</div>
+			<Planets/>
+		</>
 	);
-}; 
+};
