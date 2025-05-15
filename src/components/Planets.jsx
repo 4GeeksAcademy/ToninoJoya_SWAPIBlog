fcf6e78863e8bf1da9fb03436f92131fa212bdd1
ToninoJoya <antoniocaprab@gmail.com>
2025-05-15 23:31:04 +0000

    Logica array personajes

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 
 export const Planets = () => {
+
+
+    const getAllPlanets = async () => {
+            fetch("https://www.swapi.tech/api/planets/")
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.error(err))
+      
+    }
+
+    useEffect(() => {
+        getAllPlanets()
+    }, [])
+
 
     return (
         <div className="container mt-5">
             <h2 className="text-danger">Planets</h2>
             <div className="d-flex flex-row flex-nowrap overflow-auto py-3">
-                <div className="card me-3 flex-shrink-0" style={{ width: '18rem' }}>
+                <div className="card me-3 flex-shrink-0">
                     <img src={rigoImageUrl} className="card-img-top" alt="Card Unica" style={{ height: '150px', objectFit: 'cover' }} />
                     <div className="card-body">
                         <h5 className="card-title">Card Title</h5>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,29 +9,38 @@ export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
+	const { characters } = store
 
-
+	
+	
 	return (
 		<>
 			<div className="container mt-5">
 				<h2 className="text-danger">Characters</h2>
 				<div className="d-flex flex-row flex-nowrap overflow-auto py-3">
-					<div className="card me-3 flex-shrink-0">
-						<img src={rigoImageUrl} className="card-img-top" alt="Card Unica"/>
-						<div className="card-body">
-							{
+					{
+						characters.map((item) => {
+							return (
+								<>
 
-							}
-							<h5 className="card-title">Card Title</h5>
-							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-							<div className="d-flex justify-content-between">
-								<a href="#" className="btn btn-outline-primary">Go somewhere </a><button type="button" className="btn btn-outline-warning"><i className="fa-regular fa-heart"></i></button>
-							</div>
-						</div>
-					</div>
+									<div className="card me-3 flex-shrink-0 p-4">
+										<img src={rigoImageUrl} className="card-img-top" alt="Card Unica" />
+										<div className="card-body"></div>
+										<h5 className="card-title p-2">{item.properties.name}</h5>
+										<p className="card-text p-2">Gender: {item.properties.gender} <br /> Hair color: {item.properties.hair_color} <br /> Eye-color: {item.properties.eye_color} </p>
+										<div className="d-flex justify-content-between">
+											<a href="#" className="btn btn-outline-primary">Go somewhere </a><button type="button" className="btn btn-outline-warning"><i className="fa-regular fa-heart"></i></button>
+										</div>
+									</div>
+								</>
+							);
+						})
+					};
 				</div>
-			</div>
-			<Planets/>
+
+
+			</div >
+			<Planets />
 		</>
 	);
 };
